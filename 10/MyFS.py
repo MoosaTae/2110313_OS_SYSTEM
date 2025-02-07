88c6ea91d401539ec308aef831c63ffa2a6be44e
MoosaTae <sadhis.tae@gmail.com>
2025-02-07 20:39:59 +0700

    done

--- 10/MyFS.py
+++ 10/MyFS.py
@@ -1,98 +1,107 @@
 #!/usr/bin/env python3
- 
+
 # 2110352 Operating System
 # FUSE/Filesystem exercise
 # By Krerk Piromsopa, Ph.D. <Krerk.P@chula.ac.th>
 #    Department of Computer Engineering
 #    Chulalongkorn University.
 
-import os, stat, errno
+import os
+import stat
+import errno
 import fuse
-from fuse import Fuse
+import requests
 
-if not hasattr(fuse, '__version__'):
-    raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
+from fuse import Fuse
 
 fuse.fuse_python_api = (0, 2)
 
-containers={ \
-               '/subject':b"2021S2 - Operating Systems\nCP ENG CU\n" ,
-               '/instructors':b"0:CP ENG CU OS 2018S1 - Instructors\n" \
-b"1:      Krerk Piromsopa, Ph. D.\n" \
-b"2:      Veera Muangsin, Ph.D.\n" \
-b"3:      Thongchai Rojkangsadan\n"
-       }
+PARTICIPATION_URL = "https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/status.php"
+CHECKIN_URL = "https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/checkIn.php"
+
 
 class MyStat(fuse.Stat):
-   def __init__(self):
-       self.st_mode = 0
-       self.st_ino = 0
-       self.st_dev = 0
-       self.st_nlink = 0
-       self.st_uid = 0
-       self.st_gid = 0
-       self.st_size = 0
-       self.st_atime = 0
-       self.st_mtime = 0
-       self.st_ctime = 0
-
-class MyFS(Fuse):
-
-   def getattr(self, path):
-       st = MyStat()
-       if path == '/':
-           st.st_mode = stat.S_IFDIR | 0o777
-           st.st_nlink = 2
-       elif path in containers:
-           st.st_mode = stat.S_IFREG | 0o444
-
-           st.st_nlink = 1
-           content=containers[path]
-           st.st_size = len(content)
-       else:
-           return -errno.ENOENT
-       return st
-
-   def readdir(self, path, offset):
-       filenames=containers.keys()
-       for r in  '.', '..':
-           yield fuse.Direntry(r)
-
-       for r in filenames:
-           yield fuse.Direntry(r[1:])
+    def __init__(self):
+        self.st_mode = 0
+        self.st_ino = 0
+        self.st_dev = 0
+        self.st_nlink = 0
+        self.st_uid = 0
+        self.st_gid = 0
+        self.st_size = 0
+        self.st_atime = 0
+        self.st_mtime = 0
+        self.st_ctime = 0
 
 
-   def open(self, path, flags):
-       if path not in containers:
-           return -errno.ENOENT
-       accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-       if (flags & accmode) != os.O_RDONLY:
-           return -errno.EACCES
+class WebServiceFS(Fuse):
+    def getattr(self, path):
+        st = MyStat()
+        if path == "/":
+            st.st_mode = stat.S_IFDIR | 0o755
+            st.st_nlink = 2
+        elif path == "/participation":
+            st.st_mode = stat.S_IFREG | 0o666  # Read and write
+            content = self.myRead()
+            st.st_nlink = 1
+            st.st_size = len(content)
+        else:
+            return -errno.ENOENT
+        return st
 
-   def read(self, path, size, offset):
-       if path not in containers:
-           return -errno.ENOENT
+    def readdir(self, path, offset):
+        for entry in [".", "..", "participation"]:
+            yield fuse.Direntry(entry)
 
-       content = containers[path]
-       slen = len(content)
-       if offset < slen:
-           if offset + size > slen:
-               size = slen - offset
-           buf = content[offset:offset+size]
-       else:
-           buf = ''
-       return buf
+    def open(self, path, flags):
+        if path != "/participation":
+            return -errno.ENOENT
+        return 0
+
+    def read(self, path, size, offset):
+        if path != "/participation":
+            return -errno.ENOENT
+
+        content = self.myRead()
+        slen = len(content)
+
+        if offset < slen:
+            if offset + size > slen:
+                size = slen - offset
+            buf = content[offset : offset + size]
+        else:
+            buf = b""
+        return buf
+
+    def write(self, path, buf, offset):
+        if path != "/participation":
+            return -errno.ENOENT
+
+        written = self.myWrite(buf.decode("utf-8"))
+        return written
+
+    def myRead(self):
+        req = requests.get(PARTICIPATION_URL)
+        return req.text.encode("utf-8")
+
+    def myWrite(self, buf):
+        raw = buf.strip().split(":")
+        if len(raw) != 3:
+            return -errno.EINVAL  # Invalid argument
+
+        params = {"studentid": raw[0], "name": raw[1], "email": raw[2]}
+        requests.post(CHECKIN_URL, data=params)
+        return len(buf)
+
 
 def main():
-    usage="""
-Userspace hello example
-""" + Fuse.fusage
-    server = MyFS(version="%prog " + fuse.__version__,
-                     usage=usage,
-                     dash_s_do='setsingle')
-
+    usage = "WebService Filesystem" + Fuse.fusage
+    server = WebServiceFS(
+        version="WebServiceFS 1.0", usage=usage, dash_s_do="setsingle"
+    )
     server.parse(errex=1)
     server.main()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
